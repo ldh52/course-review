97771cb881faaa3c849092698f6458d5feab760b
Donghyun Lee <yahopet79@gmail.com>
2025-02-28 11:00:35 +0900

    build.gradle/application.properties 작성, CourseReviewApplication 작성 및 test 실행

---
+++ src/main/java/kr/co/yahopet/CoueseReviewApplicatiion.java
@@ -0,0 +1,13 @@
+package kr.co.yahopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoueseReviewApplicatiion {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoueseReviewApplicatiion.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/yahopet/CoueseReviewApplicatiionTest.java
@@ -0,0 +1,13 @@
+package kr.co.yahopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoueseReviewApplicatiionTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoueseReviewApplicatiion.class, args);
+    }
+
+}
