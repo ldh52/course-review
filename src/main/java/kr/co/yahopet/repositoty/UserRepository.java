8e3c09f66d30629922b639753ad532dd9ea8ece2
Donghyun Lee <yahopet79@gmail.com>
2025-02-28 20:36:09 +0900

    create UserRepository

---
+++ src/main/java/kr/co/yahopet/repositoty/UserRepository.java
@@ -0,0 +1,8 @@
+package kr.co.yahopet.repositoty;
+
+import kr.co.yahopet.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
