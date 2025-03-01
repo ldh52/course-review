a44cd6b7f7261549cd512fa76b9fabfffb8adb99
Donghyun Lee <yahopet79@gmail.com>
2025-03-02 00:44:38 +0900

    로그인 추가
    로그인 추가

--- src/main/java/kr/co/yahopet/service/UserServiceImpl.java
+++ src/main/java/kr/co/yahopet/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package kr.co.yahopet.service;
 import java.util.List;
 import kr.co.yahopet.entity.User;
 import kr.co.yahopet.repositoty.UserRepository;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -40,6 +41,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public int login(String uid, String pwd) {
-        return 0;
+        User user = findByUid(uid);
+        if (user == null) {
+            return USER_NOT_EXIST;
+        }
+        if (BCrypt.checkpw(pwd, user.getPwd())) {
+            return CORRECT_LOGIN;
+        }
+        return WRONG_PASSWORD;
     }
 }
